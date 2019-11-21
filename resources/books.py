b8cd9f44619c28df8a336b265618d2ffe5990081
Timm Schoenborn <timmschoenborn@gmail.com>
2019-11-21 11:10:42 -0600

    user number 1 only can edit book info

--- resources/books.py
+++ resources/books.py
@@ -42,11 +42,28 @@ def get_one_book(id):
 	return jsonify(data=book_dict, status={"code": 200, 'message':'Found book with id{}'.format(book.id)})
 
 
+#edit book info(user number 1 only)
+@books.route('/<id>', methods=['PUT'])
+@login_required
+def update_book(id):
+	payload = request.get_json()
+	book = models.Book.get_by_id(id)
 
+	if(current_user.id == 1):
+		book.title = payload['title'] if 'title' in payload else None
+		book.author = payload['author'] if 'author' in payload else None
+		book.isbn = payload['isbn'] if 'isbn' in payload else None
+		book.genre = payload['genre'] if 'genre' in payload else None
+		book.pages = payload['pages'] if 'pages' in payload else None
+		book.image = payload['image'] if 'image' in payload else None
+		book.published = payload['published'] if 'published' in payload else None
+		book.save()
 
-
-
-
+		book_dict = model_to_dict(book)
+		print(book_dict)
+		return jsonify(data=book_dict, status={"code": 200, "message": "Success"})
+	else:
+		return jsonify(data="Forbidden", status={'code': 403, 'message':"You are not authorized to edit book information"}), 403
 
 
 
