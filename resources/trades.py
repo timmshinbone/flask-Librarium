f5960d11efea0e9ecb4506eec40b62913bdd53b5
Timm Schoenborn <timmschoenborn@gmail.com>
2019-11-21 19:52:09 -0600

    trade data shows in a list and individually by id

--- resources/trades.py
+++ resources/trades.py
@@ -32,6 +32,15 @@ def make_trade_request(f, t):
 
 	return jsonify(data=trade_dict, status={"code":201, "message": "Success"}), 201
 
+
+
+
+
+
+
+
+
+
 #show all trades
 @trades.route('/', methods=['GET'])
 def show_all_trades():
@@ -49,6 +58,46 @@ def show_all_trades():
 	[x['to_user'].pop('email') for x in trades_dicts]
 
 	return jsonify(data=trades_dicts), 200
+
+#trade show one route
+@trades.route('/<id>', methods=['GET'])
+def show_one_trade(id):
+	trade = models.Trade.get_by_id(id)
+	print(trade, "this is trade")
+	trade_dict = model_to_dict(trade)
+	trade_dict['copy_from']['owner'].pop('password')
+	trade_dict['copy_from']['owner'].pop('email')
+	trade_dict['copy_to']['owner'].pop('password')
+	trade_dict['copy_to']['owner'].pop('email')
+	trade_dict['from_user'].pop('password')
+	trade_dict['from_user'].pop('email')
+	trade_dict['to_user'].pop('password')
+	trade_dict['to_user'].pop('email')
+	return jsonify(data=trade_dict, status={"code": 200, 'message':'Found copy with id{}'.format(trade.id)})
+
+#trade delete route
+@trades.route('/<id>', methods=['Delete'])
+@login_required
+def delete_trade(id):
+	trade_to_delete = models.Trade.get_by_id(id)
+	if (current_user.id != 1):
+		return jsonify(data="Forbidden", status={'code': 403, 'message':"you are not authorized to delete trades"}), 403
+	else:
+		trade_to_delete.delete_instance()
+		return jsonify(data='resource Successfully deleted', status={"code": 200, "message": "resource deleted"})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
