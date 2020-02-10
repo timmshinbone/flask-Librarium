342e8ffa40ce0a8c76c8f2918308e321b69d41ba
Timm Schoenborn <timmschoenborn@gmail.com>
2020-02-09 18:34:08 -0600

    base code for world.py get route

--- resources/world.py
+++ resources/world.py
@@ -1,3 +1,20 @@
 # Open Library example API
 # =
 # "https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&callback=mycallback"
+
+import models
+
+from flask import request, jsonify, Blueprint, redirect, url_for
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_login import login_user, current_user, logout_user, login_required
+from playhouse.shortcuts import model_to_dict
+
+
+@world.route('/', methods=['GET'])
+def get_world_api():
+	# books = models.Book.select()
+	# print(books)
+	# book_dicts = [model_to_dict(b) for b in books]
+	# all_books = list(book_dicts)
+	# return jsonify(data=all_books), 200
+
